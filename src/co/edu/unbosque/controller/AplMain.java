cd43bc03ce69bd4d1ae69137a9cbfb94cc3e511a
trueeh-slave <steamtruesnowace@gmail.com>
2022-09-21 21:42:21 -0500

    Initial commit

---
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -0,0 +1,10 @@
+package co.edu.unbosque.controller;
+
+import co.edu.unbosque.view.Window;
+
+public class AplMain {
+    public static void main(String[] args) {
+        new Controller();
+        new Window();
+    }
+}
---
+++ src/co/edu/unbosque/view/Window.java
@@ -0,0 +1,17 @@
+package co.edu.unbosque.view;
+
+import javax.swing.*;
+
+public class Window {
+    JFrame principal = new JFrame("test");
+
+    public Window(){
+        principal.setVisible(true);
+        principal.setSize(500,500);
+        principal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        principal.setLocationRelativeTo(null);
+
+        JButton btnRead = new JButton("Clic para leer el archivo");
+    }
+
+}
