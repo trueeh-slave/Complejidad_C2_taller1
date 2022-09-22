ebf65d00327b0756d75a3cb284f3dcb9b8f653b1
gmaldo03 <gmaldonados@unbosque.edu.co>
2022-09-22 12:10:50 -0500

    xd

--- src/co/edu/unbosque/view/Window.java
+++ src/co/edu/unbosque/view/Window.java
@@ -1,18 +1,50 @@
 package co.edu.unbosque.view;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class Window {
-    JFrame principal = new JFrame("test");
+public class Window extends JFrame{
+    JPanel panel = new JPanel();
+
 
     public Window(){
-        principal.setVisible(true);
-        principal.setSize(500,500);
-        principal.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        principal.setLocationRelativeTo(null);
+        setVisible(true);
+        setSize(500,500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setTitle("Prueba");
+        iniciarComponentes();
 
         JButton btnRead = new JButton("Clic para leer el archivo");
         int test;
+    }
+
+    private void iniciarComponentes(){
+
+        panel.setLayout(null);
+        this.getContentPane().add(panel);
+        botones();
+    }
+
+    private void botones(){
+
+        JButton leer = new JButton("Leer");
+        JButton kmp = new JButton("KMP");
+        JButton bp = new JButton("BP");
+        JTextArea areaTexto = new JTextArea();
+
+        leer.setBounds(50,50,100,50);
+        kmp.setBounds(200,50,100,50);
+        bp.setBounds(350,50,100,50);
+        areaTexto.setBounds(500,50,100,100);
+        areaTexto.setBackground(Color.BLACK);
+
+        panel.add(leer);
+        panel.add(kmp);
+        panel.add(bp);
+        panel.add(areaTexto);
+
+
     }
 
 }
